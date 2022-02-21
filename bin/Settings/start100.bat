8a65bc65b46ff509888affc4b1fb22d42f7a8af4
Giovani Costa <gionani@msn.com>
2022-02-21 21:51:50 +0000

    bin

---
+++ bin/Settings/start100.bat
@@ -0,0 +1,5 @@
+@echo off
+@echo 7
+.\DotsAndBoxes.exe .\settings\settings3_7.json
+@echo 8
+.\DotsAndBoxes.exe .\settings\settings3_8.json
---
+++ bin/Settings/startbc.bat
@@ -0,0 +1,5 @@
+@echo off
+@echo 9
+.\DotsAndBoxes.exe .\settings\settings3_9.json
+@echo 10
+.\DotsAndBoxes.exe .\settings\settings3_10.json
